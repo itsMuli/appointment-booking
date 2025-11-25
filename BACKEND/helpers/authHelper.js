9c7c67c322674618c01abef2950eac0c20a76326
DigitalQatalyst <dq.demo.a@digitalQatalyst.com>
2025-11-25 22:45:58 +0300

    Switch to bcryptjs for cross-platform compatibility

--- BACKEND/helpers/authHelper.js
+++ BACKEND/helpers/authHelper.js
@@ -1,9 +1,10 @@
 import bcrypt from 'bcrypt';
+import bcryptjs from 'bcryptjs';
 
 export const hashPassword = async (password) => {
     try{
         const saltRounds = 10;
-        const hashedPassword = await bcrypt.hash(password, saltRounds);
+        const hashedPassword = await bcryptjs.hash(password, saltRounds);
         return hashedPassword
     } catch (error) {
         console.log(error);
