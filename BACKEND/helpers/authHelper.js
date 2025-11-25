81d793e22cfc727793a48a16cc88253db082cbb2
DigitalQatalyst <dq.demo.a@digitalQatalyst.com>
2025-11-25 22:57:32 +0300

    Replace bcrypt with bcryptjs

--- BACKEND/helpers/authHelper.js
+++ BACKEND/helpers/authHelper.js
@@ -1,16 +1,15 @@
-import bcrypt from 'bcrypt';
 import bcryptjs from 'bcryptjs';
 
 export const hashPassword = async (password) => {
-    try{
-        const saltRounds = 10;
-        const hashedPassword = await bcryptjs.hash(password, saltRounds);
-        return hashedPassword
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    const saltRounds = 10;
+    const hashedPassword = await bcryptjs.hash(password, saltRounds);
+    return hashedPassword;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const comparePassword = async (password, hashedPassword) => {
-    return bcrypt.compare(password, hashedPassword);
-}
+  return bcryptjs.compare(password, hashedPassword);
+};
